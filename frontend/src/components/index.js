cc936284fcf5a2a35493ec855d6c9d01b7c2c195
medsriha <medsriha@gmail.com>
2025-05-12 21:30:46 -0500

    feat: create index file to export various UI components for maps, listings, and common functionalities

---
+++ frontend/src/components/index.js
@@ -0,0 +1,17 @@
+// Map Components
+export { default as MapComponent } from './map/MapComponent';
+export { default as ListingMarker } from './map/ListingMarker';
+export { default as InfoBox } from './map/InfoBox';
+
+// Listing Components
+export { default as ListingPanel } from './listings/ListingPanel';
+export { default as ImageGallery } from './listings/ImageGallery';
+export { default as ThumbnailGallery } from './listings/ThumbnailGallery';
+export { default as PropertyDetails } from './listings/PropertyDetails';
+export { default as AmenitiesSection } from './listings/AmenitiesSection';
+export { default as FinancialInfo } from './listings/FinancialInfo';
+
+// Common Components
+export { default as ActionButton } from './common/ActionButton';
+export { default as ErrorMessage } from './common/ErrorMessage';
+export { default as LoadingIndicator } from './common/LoadingIndicator'; 
